7f453523dfd5488a21dfc7cd0d055b62743dae4a
Logan Lentz <logan.lentz1@gmail.com>
2022-04-08 23:15:36 -0500

    added button and sample

---
+++ src/sample.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sample() {
+    return (
+        <div className="App">
+            <img src={require('./sample.jpg')} alt="sample image" />
+        </div>
+    );
+}
+
+export default Sample;
